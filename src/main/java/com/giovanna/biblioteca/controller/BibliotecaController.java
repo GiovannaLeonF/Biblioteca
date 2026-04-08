ba280a6c887329af152407a3125591e858885f66
Giovanna <luisagiovannaleonfuentes@gmail.com>
2026-04-08 17:34:26 -0300

    Agrego Swagger, mejoras en controller y README profesional

--- src/main/java/com/giovanna/biblioteca/controller/BibliotecaController.java
+++ src/main/java/com/giovanna/biblioteca/controller/BibliotecaController.java
@@ -1,12 +1,16 @@
 package com.giovanna.biblioteca.controller;
 import com.giovanna.biblioteca.model.Libro;
 import com.giovanna.biblioteca.repository.LibroRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/libros") //Todos los endpoints empiezan con /Libros
+@Tag(name = "Biblioteca", description = "Gestión de libros y préstamos")
 public class BibliotecaController {
 
     private final LibroRepository libroRepository;
@@ -14,26 +18,32 @@ public class BibliotecaController {
     public BibliotecaController(LibroRepository libroRepository){
         this.libroRepository = libroRepository;
     }
+    @Operation(summary = "Listar todos los libros")
     @GetMapping
     public List<Libro> listraLibro(){
         return libroRepository.findAll();
     }
     //Agregar libro nuevo
+    @Operation(summary = "Agregar un nuevo libro")
     @PostMapping
     public Libro agregarLibro(@RequestBody Libro libro){
         return libroRepository.save(libro);
     }
 
+    @Operation(summary = "Prestar un libro")
     @PutMapping("/prestar/{id}")
-    public String prestarLibro (@PathVariable Long id){
+    public ResponseEntity<String> prestarLibro (@PathVariable Long id){
         return libroRepository.findById(id).map(libro -> {
             if (libro.getStock() > 0){
                 libro.setStock(libro.getStock() -1); //descontar Stock
                 libroRepository.save(libro);
-                return "Libro prestado: " + libro.getTitulo() + ". Stock restante: " + libro.getStock();
+                return ResponseEntity.ok( "Libro prestado: " + libro.getTitulo() + ". Stock restante: " + libro.getStock()
+                );
             }else {
-                return "No hay unidades disponibles para: " + libro.getTitulo();
+                return ResponseEntity.badRequest().body("No hay unidades disponibles para: " + libro.getTitulo()
+                );
             }
-        }).orElse("Libro no encontrado con ID: " + id);
+        }).orElseGet(() ->
+                ResponseEntity.status(404).body("Libro no encontrado"));
     }
 }
